99854a89a6687fdf34fe7c79784916e3ccfe3887
Ashok Peeta <ashok.peeta@snaptravel.com>
2024-01-01 01:52:00 +0530

    null, undefined, literal types

--- everyday-types.ts
+++ everyday-types.ts
@@ -249,3 +249,84 @@ function printText(s: string, alignment: "left" | "right" | "center") {
 }
 printText("Hello, world", "left");
 // printText("G'day, mate", "centre"); Argument of type '"centre"' is not assignable to parameter of type '"left" | "right" | "center"'.
+
+// Numeric literal types work the same way
+function compare(a: string, b: string): -1 | 0 | 1 {
+  return a === b ? 0 : a > b ? 1 : -1;
+}
+
+// you can combine these with non-literal types
+interface Options {
+  width: number;
+}
+function configure(x: Options | "auto") {
+  // ...
+}
+configure({ width: 100 });
+configure("auto");
+// configure("automatic"); Argument of type '"automatic"' is not assignable to parameter of type 'Options | "auto"'.
+
+// There’s one more kind of literal type: boolean literals. There are only two boolean literal types, and as you might guess, they are the types true and false. The type boolean itself is actually just an alias for the union true | false.
+
+/*
+    Literal inference: When you initialize a variable with an object, TypeScript assumes that the properties of that object might change values later.
+*/
+
+const objs = { counter: 0 };
+if (true) {
+  objs.counter = 1;
+}
+
+// TypeScript doesn’t assume the assignment of 1 to a field which previously had 0 is an error. Another way of saying this is that obj.counter must have the type number, not 0, because types are used to determine both reading and writing behavior.
+
+// The same applies to strings:
+declare function handleRequest(url: string, method: "GET" | "POST"): void;
+
+const req = { url: "https://example.com", method: "GET" };
+// handleRequest(req.url, req.method);
+// Argument of type 'string' is not assignable to parameter of type '"GET" | "POST"'.
+
+/*
+    In the above example req.method is inferred to be string, not "GET". Because code can be evaluated between the creation of req and the call of handleRequest which could assign a new string like "GUESS" to req.method, TypeScript considers this code to have an error.
+*/
+
+// There are two ways to work around this.
+// You can change the inference by adding a type assertion in either location:
+// Change 1:
+const reqs = { url: "https://example.com", method: "GET" as "GET" };
+// Change 2
+handleRequest(reqs.url, reqs.method as "GET");
+
+// You can use as const to convert the entire object to be type literals:
+const req1 = { url: "https://example.com", method: "GET" } as const;
+handleRequest(req.url, req1.method);
+
+// The as const suffix acts like const but for the type system, ensuring that all properties are assigned the literal type instead of a more general version like string or number.
+
+/*
+    null and undefined: JavaScript has two primitive values used to signal absent or uninitialized value: null and undefined.
+
+    With strictNullChecks off, values that might be null or undefined can still be accessed normally,
+
+    With strictNullChecks on, when a value is null or undefined, you will need to test for those values before using methods or properties on that value.
+*/
+
+function doSomething(x: string | null) {
+  if (x === null) {
+    // do nothing
+  } else {
+    console.log("Hello, " + x.toUpperCase());
+  }
+}
+
+/*
+    Non-null Assertion Operator (Postfix !)
+
+    TypeScript also has a special syntax for removing null and undefined from a type without doing any explicit checking. Writing ! after any expression is effectively a type assertion that the value isn’t null or undefined:
+*/
+function liveDangerously(x?: number | null) {
+  // No error
+  console.log(x!.toFixed());
+}
+
+// Just like other type assertions, this doesn’t change the runtime behavior of your code, so it’s important to only use ! when you know that the value can’t be null or undefined.
